06d885d3a67413553c8f37607df8c4f1a5e6523d
DESKTOP-HMJ5KE0\AngelDiesel <engindenizmuhendis@gmail.com>
2023-08-21 09:59:40 +0300

    version 1.0.0

--- app/src/main/java/com/engindeniz/appupdatetest/MainActivity.java
+++ app/src/main/java/com/engindeniz/appupdatetest/MainActivity.java
@@ -1,14 +1,34 @@
 package com.engindeniz.appupdatetest;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.engindeniz.appupdatetest.databinding.ActivityMainBinding;
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        AppUpdater appUpdater = new AppUpdater(this)
+                .setUpdateFrom(UpdateFrom.JSON)
+                .setDisplay(Display.DIALOG);
+        appUpdater.start();
+
+        Log.d(TAG, "onCreate: "+binding.versionTextView.getText().toString());
     }
 }
